21ec6cac6a81746f8e4cf0804d9b6ea4af774cc3
AllyEspozo <b2029alpespinoza@pshs.edu.ph>
2026-01-13 20:39:16 +0800

    CLOCK UPDATE

---
+++ calculator/clock.js
@@ -0,0 +1,9 @@
+function updateClock() {
+  const clockEl = document.getElementById("clock");
+  if (!clockEl) return;
+
+  const now = new Date();
+  clockEl.textContent = now.toLocaleTimeString();
+}
+
+
